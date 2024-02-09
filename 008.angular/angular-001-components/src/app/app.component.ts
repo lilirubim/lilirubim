7fa9edae2b5579c0873763fa961c3687d8b13ff8
Administrator <losdientesdeava@gmail.com>
2024-02-08 19:48:16 -0800

    imports pantallas

--- 008.angular/angular-001-components/src/app/app.component.ts
+++ 008.angular/angular-001-components/src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { HolaMundoComponent } from "./hola-mundo/hola-mundo.component";
+import { AdiosMundoComponent } from './adios-mundo/adios-mundo.component';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [HolaMundoComponent, AdiosMundoComponent]
 })
 export class AppComponent {
   title = 'Hola soy Lili';
