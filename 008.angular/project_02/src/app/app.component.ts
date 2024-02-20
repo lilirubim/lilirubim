822838949c37b04b4459cac7c95f9f1b80ee4dd1
Administrator <losdientesdeava@gmail.com>
2024-02-19 20:18:42 -0800

    tables bootstrap

--- 008.angular/project_02/src/app/app.component.ts
+++ 008.angular/project_02/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { ProductListComponent } from './product-list/product-list.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
+  imports: [ProductListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- 008.angular/project_02/src/app/product-list/product-list.component.ts
+++ 008.angular/project_02/src/app/product-list/product-list.component.ts
@@ -1,12 +1,43 @@
 import { Component } from '@angular/core';
+import { Product } from '../interfaces/product.models';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-product-list',
   standalone: true,
-  imports: [],
+  imports: [NgFor],
   templateUrl: './product-list.component.html',
   styleUrl: './product-list.component.css'
 })
 export class ProductListComponent {
+
+  names: string[] = [
+    'persona1',
+    'persona2',
+    'persona3',
+    'persona4'
+  ];
+  emails:string[] = []
+
+  products: Product[] = [{
+    id: 1,
+    title: 'producto 1',
+    price: 40.50,
+    available: true
+  },
+  {
+    id: 2,
+    title: 'producto 2',
+    price: 30.50,
+    available: false
+  },
+  {
+    id: 3,
+    title: 'producto 3',
+    price: 20.50,
+    available: true
+  }
+
+];
 
 }
