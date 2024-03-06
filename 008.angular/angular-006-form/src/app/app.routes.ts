7c89661d4ab24f5a2d5262036163fb6aa76407c7
lilirubim <148784760+lilirubim@users.noreply.github.com>
2024-03-05 18:31:21 -0800

    form input

--- 008.angular/angular-006-form/src/app/app.routes.ts
+++ 008.angular/angular-006-form/src/app/app.routes.ts
@@ -1,3 +1,34 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { BookListComponent } from './book-list/book-list.component';
+import { BookDetailComponent } from './book-detail/book-detail.component';
+import { HomeComponent } from './home/home.component';
+import { BookFormComponent } from './book-form/book-form.component';
+import { RegisterComponent } from './register/register.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: HomeComponent
+    },
+    {
+        path: 'books',
+        component: BookListComponent
+    },
+    {
+        path: 'books/:id/detalle',
+        component: BookDetailComponent
+    },
+    {
+        path: 'books/create',
+        component: BookFormComponent
+    },
+    {
+        path: 'login',
+        component: LoginComponent
+    },
+    {
+        path: 'register',
+        component: RegisterComponent
+    }
+];
--- 008.angular/angular-006-form/src/app/book-form/book-form.component.ts
+++ 008.angular/angular-006-form/src/app/book-form/book-form.component.ts
@@ -1,12 +1,48 @@
+import { HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
+import { Book } from '../models/book.model';
 
 @Component({
   selector: 'app-book-form',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, HttpClientModule],
   templateUrl: './book-form.component.html',
   styleUrl: './book-form.component.css'
 })
 export class BookFormComponent {
+  
+  bookForm = this.fb.group({
+    id: [0],
+    title: [''],
+    price: [0.0],
+    available: [false],
+    publishDate: [new Date()]
+  });
 
+  constructor(private fb: FormBuilder) {}
+  save (){
+    console.log("Guardando book");
+
+      // Extraer los valores de cada input escritos por el usuario
+    const id = this.bookForm.get('id')?.value ?? 0;
+    const title = this.bookForm.get('title')?.value ?? 'título por defecto';
+    const price = this.bookForm.get('price')?.value ?? 0.0;
+    const available = this.bookForm.get('available')?.value ?? false;
+    const publishDate = this.bookForm.get('publishDate')?.value ?? new Date;
+
+    // Crear un objeto utilizando los valores extraìdos
+    const bookToSave: Book = {
+      id: id,
+      title: title,
+      price: price,
+      available: available,
+      publishDate: publishDate
+    }
+    console.log(bookToSave);
+
+    // Enviar el objeto a backend utilizando HttpClient
+
+
+  }
 }
